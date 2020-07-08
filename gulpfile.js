122e87ba3e29764a02c6d16e43091ed871e0793e
from0to1-nk <skykerch@live.com>
2020-07-08 10:33:35 +0300

    added search and products

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ let gulp = require('gulp'),
 gulp.task('sass', function () {
     return gulp.src('app/scss/**/*.scss')
         .pipe(sass({
-            outputStyle: 'compressed'
+            outputStyle: 'expanded'
         }))
         .pipe(rename({
             suffix: '.min'
@@ -35,7 +35,7 @@ gulp.task('style', function () {
 });
 gulp.task('script', function () {
     return gulp.src([
-
+            'node_modules/mixitup/dist/mixitup.js',
             'node_modules/slick-carousel/slick/slick.js',
             'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
         ])
