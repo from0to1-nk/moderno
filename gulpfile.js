7df0f153e7f8154dc2bb99edd3884d186bfed27f
from0to1-nk <skykerch@live.com>
2020-07-19 01:03:32 +0300

    addet about, 404, contacts

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,8 @@ gulp.task('style', function () {
             'node_modules/slick-carousel/slick/slick.css',
             'node_modules/magnific-popup/dist/magnific-popup.css',
             'node_modules/rateyo/lib/cjs/jquery.rateyo.css',
-            'node_modules/ion-rangeslider/css/ion.rangeSlider.css'
+            'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
+            'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css'
         ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
@@ -41,7 +42,8 @@ gulp.task('script', function () {
             'node_modules/slick-carousel/slick/slick.js',
             'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
             'node_modules/rateyo/lib/cjs/jquery.rateyo.js',
-            'node_modules/ion-rangeslider/js/ion.rangeSlider.js'
+            'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+            'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js'
         ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
